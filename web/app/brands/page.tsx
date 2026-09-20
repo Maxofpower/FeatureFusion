58de3916a893decd2e5369295a3a578553f8fddb
ArkadiNazarian <nazarianarkadi@gmail.com>
2026-09-20 17:37:20 +0330

    feat(web): make the brand page to be SSR

--- web/app/brands/page.tsx
+++ web/app/brands/page.tsx
@@ -3,7 +3,9 @@ import type { Brand } from "@/models"
 import { BrandsPage } from "./_components/brands"
 
 export default async function Brands() {
-  const getBrands = await fetcher<Brand[]>(`/catalog/brands`)
+  const getBrands = await fetcher<Brand[]>(`/catalog/brands`, {
+    cache: 'no-store'
+  })
 
   if (!getBrands.success || getBrands.data === undefined) {
     return <div>Error: {getBrands.message}</div>
